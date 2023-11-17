24a529617a7a6e359d977ec8c25ae83b790d97d7
Natalia Sabadysh <natalia@Tests-MacBook-Pro.local>
2023-11-17 13:25:22 -0800

    OpenAI API,  Semantic Search, Mongodb, Next.js-  created chat bot documented with templates

---
+++ README.md
@@ -0,0 +1,19 @@
+# OpenAI + Semantic Search
+
+# Let AI Be Your Docs
+
+
+# STACK: 
+
+## 1. Mongodb Atlas
+1. Full-text search - Atlas Search provides you with full-text search capabilities, including stemming, stop words, and synonyms.
+2. Scoring - Atlas Search provides you with a scoring system that allows you to rank your search results based on relevance.
+3. Language support - Atlas Search supports multiple languages out of the box.
+4. Autocomplete - Atlas Search provides you with autocomplete capabilities.
+5. Highlighting - Atlas Search provides you with highlighting capabilities.
+
+## 2. OpenAI
+## 3. Next.Js
+
+# API
+[OpenAI](https://platform.openai.com/api-keys)
---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useChat } from "ai/react";
+import ReactMarkdown from "react-markdown";
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  return (
+    <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
+      {messages.length > 0
+        ? messages.map((m) => (
+            <div key={m.id} className="whitespace-pre-wrap">
+              <div className={`message ${m.role=== "user" ? 'user-msg' : 'ai-msg'}`}>{m.role === "user" ? "User: " : "AI: "}
+              <div className="message-content"> <ReactMarkdown>{m.content}</ReactMarkdown></div>
+              </div>
+            </div>
+          ))
+        : null}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
+}
